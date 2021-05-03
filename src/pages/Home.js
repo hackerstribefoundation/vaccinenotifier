cb36a383f9c077d45d34186db0dc265bee2633c1
Rishabh Banga <rishabhbangaa@gmail.com>
2021-05-03 12:05:52 +0530

    Revert "Update Home.js"
    This reverts commit 1b8dd5f6616826d74100c264317a9decc3b21598.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,6 +13,19 @@ import Container from '@material-ui/core/Container';
 import {IconButton, Snackbar, withStyles} from "@material-ui/core";
 import {CloseRounded, LockOutlined} from "@material-ui/icons";
 
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://hackerstribe.com/">
+                Hackerstribe
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
 const useStyles =(theme) => ({
     paper: {
         marginTop: theme.spacing(8),
@@ -163,9 +176,15 @@ class Home extends Component{
                                 className={classes.submit}
                                 onClick={pincodeIdIncrementer}
                             >
-                                Add More Pincodes
+                                Add Pincodes
                             </Button>
 
+                            <Grid item xs={12}>
+                                <FormControlLabel
+                                    control={<Checkbox value="allowExtraEmails" color="primary" />}
+                                    label="I want to receive updates and notifications via email."
+                                />
+                            </Grid>
                         </Grid>
                         <Button
                             type="submit"
